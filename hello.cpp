8307b72a64993f10d8c0ce898c2c08d5036a8ed2
jcincotta22 <jcincotta22@gmail.com>
2016-12-09 17:05:32 -0500

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// main() is where program execution begins.
+
+int main() {
+  int a;
+  a = 10 * 10;
+   cout << a * 5; // prints Hello World
+   return 0;
+}
